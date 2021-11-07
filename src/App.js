b521836cd3865aea1de3bf861af3799b1c356a4f
Euan McMenemin <euanmcmen@hotmail.com>
2021-11-07 17:54:46 +0000

    request to my api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row } from "react-bootstrap";
 
 function App() {
+  const getText = async () => {
+    var requestOptions = {
+      method: "POST",
+    };
+
+    var requestUrl = "https://localhost:44354/api/PetFeeds";
+
+    var response = await fetch(requestUrl, requestOptions);
+
+    var data = await response.json();
+
+    console.log(data);
+
+    const stringyData = JSON.stringify(data);
+
+    setText(stringyData);
+  };
+
+  const [text, setText] = useState("");
+
   return (
     <>
       <Container>
-        <h1 className="text-center">Welcome to my Habitica Pet Feeder App</h1>
+        <Row>
+          <h1 className="text-center">Welcome to my Habitica Pet Feeder App</h1>
+        </Row>
+        <Row>
+          <button onClick={() => getText()}>Get Pet Feeds</button>
+        </Row>
+        <Row>
+          <span>{text}</span>
+        </Row>
       </Container>
     </>
   );
