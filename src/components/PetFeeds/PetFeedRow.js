0260f860c193043173d8a9ac1420ea94b5e3ee80
Euan McMenemin <euanmcmen@hotmail.com>
2021-12-09 09:02:36 +0000

    one image per feed quantity

--- src/components/PetFeeds/PetFeedRow.js
+++ src/components/PetFeeds/PetFeedRow.js
@@ -1,14 +1,10 @@
 import { Col, Row, ListGroup, Badge, Figure, Image } from "react-bootstrap";
 
 const PetFeed = (props) => {
-  function getSatisfiedText(isSatisfied) {
-    return isSatisfied ? "will be satisfied." : "will want more food.";
-  }
-
   return (
     <ListGroup.Item key={`${props.petFullName}+${props.foodFullName}`}>
-      <Row>
-        <Col>
+      <Row className="justify-content-md-center">
+        <Col md="auto">
           <Image
             src={`https://habitica-assets.s3.amazonaws.com/mobileApp/images/Pet-${props.petFullName}.png`}
             alt={props.petFullName}
@@ -18,18 +14,18 @@ const PetFeed = (props) => {
           />
         </Col>
         <Col>
-          <Image
-            src={`https://habitica-assets.s3.amazonaws.com/mobileApp/images/Pet_Food_${props.foodFullName}.png`}
-            alt={props.petFullName}
-            width={100}
-            height={100}
-            roundedCircle
-          />
-        </Col>
-        <Col>
-          <Badge variant="primary" pill>
-            {props.feedQuantity}
-          </Badge>
+          <span>
+            {[...Array(props.feedQuantity)].map((element, index) => (
+              <Image
+                key={index}
+                src={`https://habitica-assets.s3.amazonaws.com/mobileApp/images/Pet_Food_${props.foodFullName}.png`}
+                alt={props.petFullName}
+                width={100}
+                height={100}
+                roundedCircle
+              />
+            ))}
+          </span>
         </Col>
       </Row>
     </ListGroup.Item>
